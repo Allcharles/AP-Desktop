8a43e1dc02df8c753c6595eb267dd93d5e3b5426
Allcharles <charles@alleman.com.au>
2019-01-20 15:46:24 +1000

    Formatting

--- AP_Desktop/src/js/AP.js
+++
@@ -1,6 +0,0 @@
-export function listAPCommands() {
-    var python = require('child_process').spawn('AnalysisPrograms.exe', ['list']);
-    python.stdout.on('data',function(data){
-        console.log("data: ",data.toString('utf8'));
-    });
-}
---
+++ AP_Desktop/src/js/AnalysisHelper.js
@@ -0,0 +1,6 @@
+function listAPCommands() {
+  var python = require("child_process").spawn("AnalysisPrograms.exe", ["list"]);
+  python.stdout.on("data", function(data) {
+    console.log("data: ", data.toString("utf8"));
+  });
+}
