89483c83161cd2023a006b31531c2d4765cb909e
Allcharles <charles@alleman.com.au>
2019-03-06 14:18:03 +1100

    Powershell File

---
+++ src/pwsh/apDownload.ps1
@@ -0,0 +1,5 @@
+# Windows
+curl.exe -O https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ../../ap/download_ap.ps1
+./download_ap.ps1 -package Weekly -destination ../../ap
+
+# Linux
