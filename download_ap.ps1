64cda92dd0a040faca4f02d399628f4c851f3795
Allcharles <charles@alleman.com.au>
2019-03-07 15:47:17 +1100

    Removed Need to Specify Linux Computer

--- download_ap.ps1
+++ download_ap.ps1
@@ -1,21 +1,18 @@
-param(
-  [Parameter(Mandatory = $true)][bool]$windows
-)
+# Delete old AP files
+Remove-Item ./ap -Recurse
+New-Item -Path ./ap -ItemType Directory
 
-if ($windows) {
-  # Windows
-  Remove-Item ./ap -Recurse
-  New-Item -Path ./ap -ItemType Directory
-  curl.exe https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
-  ./ap/download_ap.ps1 -package Weekly -destination ./ap
-  Remove-Item -Path ./ap/download_ap.ps1
-}
-else {
-  # Linux
-  Remove-Item ./ap -Recurse
-  New-Item -Path ./ap -ItemType Directory
-  curl https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
+# Get AP Downloader
+curl.exe https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
+
+# Check if not linux
+if ([System.Environment]::OSVersion.Platform -ne "Win32NT") {
+  Write-Output("Linux Detected")
   chmod +x ./download_ap.ps1
-  ./ap/download_ap.ps1 -package Weekly -destination ./ap
-  Remove-Item -Path ./ap/download_ap.ps1
 }
+
+# Extract AP to folder
+./ap/download_ap.ps1 -package Weekly -destination ./ap
+
+#Remove AP Downloader
+Remove-Item -Path ./ap/download_ap.ps1
