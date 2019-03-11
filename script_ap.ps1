eff25ea015223075bbad073160bd0ccca4563948
Charles Alleman <charles@businessinmind.com.au>
2019-03-11 14:31:28 +1000

    Updated Build Script
    Working on Linux, AP download however is not working at all.

--- script_ap.ps1
+++ script_ap.ps1
@@ -1,21 +1,19 @@
 # Delete old AP files
-Remove-Item ./ap -Recurse
-New-Item -Path ./ap -ItemType Directory
+New-Item -Path ./ap -ItemType Directory -Force
 
-# Stops system from downloading broken file
-<# # Get AP Downloader
+# Get AP Downloader
 # Check if Linux
 if ([System.Environment]::OSVersion.Platform -ne "Win32NT") {
   Write-Output("Linux Detected")
-  curl https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./download_ap.ps1
-  chmod +x ./download_ap.ps1
+  curl https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
+  chmod +x ./ap/download_ap.ps1
 } else {
   Write-Output("Windows Detected")
-  curl.exe https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./download_ap.ps1
-} #>
+  curl.exe https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
+}
 
 # Extract AP to folder
-./download_ap.ps1 -package Weekly -destination ./ap
+./ap/download_ap.ps1 -package Weekly -destination ./ap
 
 #Remove AP Downloader
-# Remove-Item -Path ./download_ap.ps1
+ Remove-Item -Path ./ap/download_ap.ps1
