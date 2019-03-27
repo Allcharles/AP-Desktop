4eaaec872bade1ad5ed98c70f603fdc3c27656da
Charles Alleman <charles@businessinmind.com.au>
2019-03-27 17:48:23 +1000

    Basic Navigation Unit Tests Added

--- tests/test.js
+++ tests/test.js
@@ -29,7 +29,7 @@ global.before(function() {
 });
 /* Boilerplate End */
 
-describe("Test Example", function() {
+describe("Basic Functionality", function() {
 	beforeEach(function() {
 		return app.start();
 	});
@@ -38,17 +38,60 @@ describe("Test Example", function() {
 		return app.stop();
 	});
 
-	it("Opens a Window", function() {
+	//Checks initial window is opening correctly
+	it("Check window opens", function() {
 		return app.client
 			.waitUntilWindowLoaded()
 			.getWindowCount()
-			.should.eventually.equal(1);
+			.should.eventually.have.at.least(1);
 	});
 
-	it("Tests the Title", function() {
+	//Checks title of App if AP Desktop
+	it("Check for correct title", function() {
 		return app.client
 			.waitUntilWindowLoaded()
 			.getTitle()
 			.should.eventually.equal("AP Desktop");
+	});
+
+	//Check initial tab is the analysis tab
+	it("Check initial tab is Analysis", function() {
+		return (
+			app.client.waitUntilWindowLoaded().element("#page").innerHTML ===
+			"Analysis"
+		);
+	});
+
+	//Check output tab is the middle tab and navigatable
+	it("Check output tab navigation", function() {
+		app.client
+			.waitUntilWindowLoaded()
+			.element("#output")
+			.click();
+		return (
+			app.client.waitUntilWindowLoaded().element("#page").innerHTML === "Output"
+		);
+	});
+
+	//Check output tab is the middle tab and navigatable
+	it("Check utilities tab navigation", function() {
+		app.client
+			.waitUntilWindowLoaded()
+			.element("#utilities")
+			.click();
+		return (
+			app.client.waitUntilWindowLoaded().element("#page").innerHTML ===
+			"Utilities"
+		);
+	});
+});
+
+describe("Terminal Check", function() {
+	beforeEach(function() {
+		return app.start();
+	});
+
+	afterEach(function() {
+		return app.stop();
 	});
 });
