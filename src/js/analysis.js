78d16a6f4a67e9e2a7553caa120017f6b5eff725
Charles Alleman <charles@businessinmind.com.au>
2019-03-28 14:47:03 +1000

    Fixed audio2csv adv opt offset
    Turned start and end offset into one input. Not tested yet

--- src/js/analysis.js
+++ src/js/analysis.js
@@ -2,237 +2,236 @@
  * Command object for abstracting the creation of terminal commands using AP.
  */
 class APCommand {
-	/**
-	 * Constructor for the command. This generates a terminal command using AP
-	 * @param {string} type Command type to run
-	 * @param {[AnalysisOption]} options List of AP command options (Optional)
-	 */
-	constructor(type, options = null) {
-		this.type = type;
-		this.options = options;
+  /**
+   * Constructor for the command. This generates a terminal command using AP
+   * @param {string} type Command type to run
+   * @param {[AnalysisOption]} options List of AP command options (Optional)
+   */
+  constructor(type, options = null) {
+    this.type = type;
+    this.options = options;
 
-		this.toString = function() {
-			return `${this.type} ${this.options}`;
-		};
-	}
+    this.toString = function() {
+      return `${this.type} ${this.options}`;
+    };
+  }
 
-	/**
-	 * Returns a terminal from the Terminal class runniig the AP command
-	 * @returns Spawn
-	 */
-	getTerminal() {
-		//Create args and append command
-		let args = [];
-		args.push(this.type);
+  /**
+   * Returns a terminal from the Terminal class runniig the AP command
+   * @returns Spawn
+   */
+  getTerminal() {
+    //Create args and append command
+    let args = [];
+    args.push(this.type);
 
-		//Append option names and values
-		if (this.options != null) {
-			this.options.forEach(option => {
-				args.push(option.toString());
-			});
-		}
+    //Append option names and values
+    if (this.options != null) {
+      this.options.forEach(option => {
+        args.push(option.toString());
+      });
+    }
 
-		return Terminal.createAPTerminal(args);
-	}
+    return Terminal.createAPTerminal(args);
+  }
 }
 
 /**
  * Analysis Option object for abstracting the creation of AP options.
  */
 class AnalysisOption {
-	/**
-	 * Object class for analysis options including flags, values, and flag values.
-	 * @param {string} option Analysis option
-	 * @param {string} value Analysis option value (Optional)
-	 */
-	constructor(option, value = null) {
-		this.option = option;
-		this.value = value;
+  /**
+   * Object class for analysis options including flags, values, and flag values.
+   * @param {string} option Analysis option
+   * @param {string} value Analysis option value (Optional)
+   */
+  constructor(option, value = null) {
+    this.option = option;
+    this.value = value;
 
-		this.toString = function() {
-			if (this.value !== null) return `${this.option}=${this.value}`;
-			else return `${this.option}`;
-		};
-	}
+    this.toString = function() {
+      if (this.value !== null) return `${this.option}=${this.value}`;
+      else return `${this.option}`;
+    };
+  }
 }
 
 /**
  * Analysis object for abstracting the creation of AP analyses.
  */
 class APAnalysis extends APCommand {
-	/**
-	 * Constructor for the analysis. This generates an AP command
-	 * @param {string} type Analysis type to run
-	 * @param {string} source Audio file path
-	 * @param {string} config Config file path
-	 * @param {string} output Output folder path
-	 * @param {[AnalysisOption]} options List of AP command options (Optional)
-	 */
-	constructor(type, source, config, output, options = null) {
-		//Append options to new array
-		let combinedOptions = [];
-		combinedOptions.push(new AnalysisOption(source));
-		combinedOptions.push(new AnalysisOption(config));
-		combinedOptions.push(new AnalysisOption(output));
+  /**
+   * Constructor for the analysis. This generates an AP command
+   * @param {string} type Analysis type to run
+   * @param {string} source Audio file path
+   * @param {string} config Config file path
+   * @param {string} output Output folder path
+   * @param {[AnalysisOption]} options List of AP command options (Optional)
+   */
+  constructor(type, source, config, output, options = null) {
+    //Append options to new array
+    let combinedOptions = [];
+    combinedOptions.push(new AnalysisOption(source));
+    combinedOptions.push(new AnalysisOption(config));
+    combinedOptions.push(new AnalysisOption(output));
 
-		//If options given
-		if (options !== null) {
-			//Push options to array
-			combinedOptions.push.apply(combinedOptions, options);
-		}
+    //If options given
+    if (options !== null) {
+      //Push options to array
+      combinedOptions.push.apply(combinedOptions, options);
+    }
 
-		super(type, combinedOptions);
-		this.type = type;
-		this.source = source;
-		this.config = config;
-		this.output = output;
-		this.terminalOutput = output;
-	}
+    super(type, combinedOptions);
+    this.type = type;
+    this.source = source;
+    this.config = config;
+    this.output = output;
+    this.terminalOutput = output;
+  }
 
-	/**
-	 * Determines whether an input is a FlagWithValue based on its classList
-	 * @param {DOMTokenList} flagList List of classNames
-	 * @returns {boolean} True if input is a FlagWithValue
-	 */
-	static isFlagsWithValues(flagList) {
-		for (let i = 0; i < flagList.length; i++) {
-			if (flagList[i] === "flagsWithValues") {
-				return true;
-			}
-		}
+  /**
+   * Determines whether an input is a FlagWithValue based on its classList
+   * @param {DOMTokenList} flagList List of classNames
+   * @returns {boolean} True if input is a FlagWithValue
+   */
+  static isFlagsWithValues(flagList) {
+    for (let i = 0; i < flagList.length; i++) {
+      if (flagList[i] === "flagsWithValues") {
+        return true;
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	/**
-	 * Returns the analysis type to run
-	 * @returns {String} Analysis type to run
-	 */
-	getType() {
-		return this.type;
-	}
+  /**
+   * Returns the analysis type to run
+   * @returns {String} Analysis type to run
+   */
+  getType() {
+    return this.type;
+  }
 
-	/**
-	 * Returns the audio file to be processed
-	 * @returns {string} Full filepath of audio file to process
-	 */
-	getSource() {
-		return this.source;
-	}
+  /**
+   * Returns the audio file to be processed
+   * @returns {string} Full filepath of audio file to process
+   */
+  getSource() {
+    return this.source;
+  }
 
-	/**
-	 * Returns the config file to be processed
-	 * @returns {string} Full filepath of config file to process
-	 */
-	getConfig() {
-		return this.config;
-	}
+  /**
+   * Returns the config file to be processed
+   * @returns {string} Full filepath of config file to process
+   */
+  getConfig() {
+    return this.config;
+  }
 
-	/**
-	 * Returns the output folder to be processed
-	 * @returns {string} Full filepath of output folder
-	 */
-	getOutput() {
-		return this.output;
-	}
+  /**
+   * Returns the output folder to be processed
+   * @returns {string} Full filepath of output folder
+   */
+  getOutput() {
+    return this.output;
+  }
 
-	/**
-	 * Set final output which is determined at runtime by AP
-	 * @param {string} terminalOutput Output for analysis files
-	 */
-	setTerminalOutput(terminalOutput) {
-		this.terminalOutput = terminalOutput;
-	}
+  /**
+   * Set final output which is determined at runtime by AP
+   * @param {string} terminalOutput Output for analysis files
+   */
+  setTerminalOutput(terminalOutput) {
+    this.terminalOutput = terminalOutput;
+  }
 
-	/**
-	 * Returns the output folder determined at runtime by AP
-	 * @returns {string} Output location of analysis files
-	 */
-	getTerminalOutput() {
-		return this.terminalOutput;
-	}
+  /**
+   * Returns the output folder determined at runtime by AP
+   * @returns {string} Output location of analysis files
+   */
+  getTerminalOutput() {
+    return this.terminalOutput;
+  }
 }
 
 /**
  * Creates a CheckEnvironment analysis object
  */
 class CheckEnvironment extends APCommand {
-	/**
-	 * Constructor for the analysis. This generates a CheckEnvironment AP Command.
-	 */
-	constructor() {
-		super("CheckEnvironment");
-	}
+  /**
+   * Constructor for the analysis. This generates a CheckEnvironment AP Command.
+   */
+  constructor() {
+    super("CheckEnvironment");
+  }
 }
 
 /**
  * Creates an audio2csv analysis object.
  */
 class Audio2CSVAnalysis extends APAnalysis {
-	/**
-	 * Constructor for the Audio2csv object. This generates an audio2csv AP command.
-	 * @param {string} source Audio file path
-	 * @param {string} config Config file path
-	 * @param {string} output Output file path
-	 * @param {[AnalysisOption]} options List of AP command options
-	 */
-	constructor(source, config, output, options = null) {
-		super("audio2csv", source, config, output, options);
-	}
+  /**
+   * Constructor for the Audio2csv object. This generates an audio2csv AP command.
+   * @param {string} source Audio file path
+   * @param {string} config Config file path
+   * @param {string} output Output file path
+   * @param {[AnalysisOption]} options List of AP command options
+   */
+  constructor(source, config, output, options = null) {
+    super("audio2csv", source, config, output, options);
+  }
 
-	/**
-	 * Takes in an empty list of analysis options, searches the form for selected advanced options, and adds the options to the list.
-	 * @param {[AnalysisOption]} finalOptions Empty list of analysis options
-	 */
-	static getOptions() {
-		const form = document.getElementById("AnalysisForm");
-		const SWITCH = 0,
-			INPUT = 1,
-			SELECT = 2;
-		const AUDIO2CSV_ADVANCED = [
-			["analysis-identifier", SELECT],
-			["temp-dir", INPUT],
-			["start-offset", INPUT],
-			["end-offset", INPUT],
-			["align-to-minute", SELECT],
-			["channels", INPUT],
-			["mix-down-to-mono", SWITCH],
-			["parallel", SWITCH],
-			["when-exit-copy-log", SWITCH],
-			["when-exit-copy-config", SWITCH],
-			["log-level", SELECT]
-		];
+  /**
+   * Takes in an empty list of analysis options, searches the form for selected advanced options, and adds the options to the list.
+   * @param {[AnalysisOption]} finalOptions Empty list of analysis options
+   */
+  static getOptions() {
+    const form = document.getElementById("AnalysisForm");
+    const SWITCH = 0,
+      INPUT = 1,
+      SELECT = 2;
+    const AUDIO2CSV_ADVANCED = [
+      ["temp-dir", INPUT],
+      ["start-offset", INPUT],
+      ["end-offset", INPUT],
+      ["align-to-minute", SELECT],
+      ["channels", INPUT],
+      ["mix-down-to-mono", SWITCH],
+      ["parallel", SWITCH],
+      ["when-exit-copy-log", SWITCH],
+      ["when-exit-copy-config", SWITCH],
+      ["log-level", SELECT]
+    ];
 
-		//Reset variable
-		let finalOptions = [];
+    //Reset variable
+    let finalOptions = [];
 
-		//Find all attached audio2csv options
-		AUDIO2CSV_ADVANCED.forEach(checkbox => {
-			let item = form.querySelector(`#${checkbox[0]}`);
+    //Find all attached audio2csv options
+    AUDIO2CSV_ADVANCED.forEach(checkbox => {
+      let item = form.querySelector(`#${checkbox[0]}`);
 
-			//If item is a true/false input
-			if (checkbox[1] === SWITCH) {
-				if (this.isFlagsWithValues(item.classList)) {
-					//FlagWithValue item
-					finalOptions.push(
-						new AnalysisOption(
-							`${item.value}=${item.checked ? "True" : "False"}`
-						)
-					);
-				} else if (item.checked) {
-					finalOptions.push(new AnalysisOption(item.value));
-				}
-			} else if (item.checked) {
-				let input = form.querySelector(`#${checkbox[0]}-input`);
-				finalOptions.push(new AnalysisOption(item.value, input.value));
-			}
-		});
+      //If item is a true/false input
+      if (checkbox[1] === SWITCH) {
+        if (this.isFlagsWithValues(item.classList)) {
+          //FlagWithValue item
+          finalOptions.push(
+            new AnalysisOption(
+              `${item.value}=${item.checked ? "True" : "False"}`
+            )
+          );
+        } else if (item.checked) {
+          finalOptions.push(new AnalysisOption(item.value));
+        }
+      } else if (item.checked) {
+        let input = form.querySelector(`#${checkbox[0]}-input`);
+        finalOptions.push(new AnalysisOption(item.value, input.value));
+      }
+    });
 
-		//If no options were selected, reset to null
-		if (finalOptions.length === 0) {
-			finalOptions = null;
-		}
+    //If no options were selected, reset to null
+    if (finalOptions.length === 0) {
+      finalOptions = null;
+    }
 
-		return finalOptions;
-	}
+    return finalOptions;
+  }
 }
--- src/js/toggleables.js
+++ src/js/toggleables.js
@@ -3,11 +3,11 @@
  * @param el {HTMLElement} Title element
  */
 function toggleImage(el) {
-	let img = document.querySelector("#div" + el.id.substr(3));
+  let img = document.querySelector("#div" + el.id.substr(3));
 
-	img.style.display === "none"
-		? (img.style.display = "inherit")
-		: (img.style.display = "none");
+  img.style.display === "none"
+    ? (img.style.display = "inherit")
+    : (img.style.display = "none");
 }
 
 /**
@@ -15,16 +15,16 @@ function toggleImage(el) {
  * @param el {HTMLElement} Title element
  */
 function toggleTerminal(el) {
-	let terminal = document.querySelector("#div" + el.id.substr(3));
-	let parent = terminal.parentElement;
+  let terminal = document.querySelector("#div" + el.id.substr(3));
+  let parent = terminal.parentElement;
 
-	if (terminal.style.display === "none") {
-		terminal.style.display = "inherit";
-		parent.style.marginBottom = "0px";
-	} else {
-		terminal.style.display = "none";
-		parent.style.marginBottom = "-28px";
-	}
+  if (terminal.style.display === "none") {
+    terminal.style.display = "inherit";
+    parent.style.marginBottom = "0px";
+  } else {
+    terminal.style.display = "none";
+    parent.style.marginBottom = "-28px";
+  }
 }
 
 /**
@@ -32,23 +32,40 @@ function toggleTerminal(el) {
  * @param {HTMLElement} el Header element
  */
 function toggleHeader(el) {
-	let body = el.parentElement.lastElementChild;
+  let body = el.parentElement.lastElementChild;
 
-	body.style.display === "none"
-		? (body.style.display = "inherit")
-		: (body.style.display = "none");
+  body.style.display === "none"
+    ? (body.style.display = "inherit")
+    : (body.style.display = "none");
 }
 
 /**
  * Toggles disability property of associated input. Associated input id is the same id as el plus '-input'.
- * Eg. el id="test", associated input id = "test-input".
+ * Eg. el id="test", associated input id="test-input".
+ * Exeption to this rule is a multi-input option which occurs when two AP inputs are connected. The value of the
+ * label element determines the name of the input Eg. el id="test" value="-s:-e", associated inputs
+ * id="test-input-s" and id="test-input-e".
  * @param {HTMLElement} el Checkbox which toggles editability of associated input
  */
 function toggleInput(el) {
-	let id = `${el.id}-input`;
+  //Check if multi input value
+  if (el.value.includes(":")) {
+    //Get list of inputs
+    let inputs = el.value.split(":").map(input => `${el.id}-input${input}`);
 
-	//If item is checked, enable input. Else disable
-	el.checked
-		? (document.getElementById(id).disabled = false)
-		: (document.getElementById(id).disabled = true);
+    //For each input, if item is checked enable input. Else disable
+    inputs.forEach(id => {
+      el.checked
+        ? (document.getElementById(id).disabled = false)
+        : (document.getElementById(id).disabled = true);
+    });
+  } else {
+    //Single input value
+    let id = `${el.id}-input`;
+
+    //If item is checked, enable input. Else disable
+    el.checked
+      ? (document.getElementById(id).disabled = false)
+      : (document.getElementById(id).disabled = true);
+  }
 }
