9020bfb55228a1abcfd0762183fa2e5dd945fb83
Charles Alleman <charles@businessinmind.com.au>
2019-05-02 10:52:40 +1000

    Initial TravisCI Script

--- scripts/linux_dependencies.sh
+++ scripts/linux_dependencies.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo apt install mono-complete ffmpeg wavpack libsox-fmt-all sox shntool mp3splt libav-tools
 sudo ln -s /usr/bin/ffmpeg /usr/local/bin/ffmpeg || true
 sudo ln -s /usr/bin/ffprobe /usr/local/bin/ffprobe || true
