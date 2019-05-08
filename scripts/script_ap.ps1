c911cd880a5541d31f63674bb1d466ea9427f4d2
Allcharles <charles@alleman.com.au>
2019-05-08 12:03:32 +1000

    Added Azure Pipelines

--- scripts/script_ap.ps1
+++ scripts/script_ap.ps1
@@ -1,24 +1,22 @@
 # Set environment
 [System.Environment]::SetEnvironmentVariable('LANG', 'en_US.UTF-8')
-echo "Downloading latest version of AP for Release. This could take up to 15 minutes."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Write-Output "Downloading latest version of AP for Release. This could take up to 15 minutes."
 
 # Delete old AP files
 New-Item -Path ./ap -ItemType Directory -Force
 
 # Get AP Downloader
-# Check if Linux
+Invoke-WebRequest https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
+  
+# Enable execute permission on linux or mac
 if ([System.Environment]::OSVersion.Platform -ne "Win32NT") {
-  Write-Output("Linux Detected")
-  curl https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
   chmod +x ./ap/download_ap.ps1
-}
-else {
-  Write-Output("Windows Detected")
-  curl.exe https://raw.githubusercontent.com/QutEcoacoustics/audio-analysis/master/build/download_ap.ps1 -o ./ap/download_ap.ps1
 }
 
 # Extract AP to folder
-./ap/download_ap.ps1 -package Weekly -destination ./ap -github_api_token 2dbc70b0e422fcba4af344182de8cb95635dfac0
+./ap/download_ap.ps1 -package Weekly -destination ./ap
 
 #Remove AP Downloader
 Remove-Item -Path ./ap/download_ap.ps1
