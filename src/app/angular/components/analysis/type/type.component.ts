769d2d0cb2722b90abc9ad876a9730031967780d
Charles Alleman <charles@alleman.com.au>
2020-01-13 15:49:35 +1000

    Added analysis type html

--- src/app/angular/components/analysis/type/type.component.ts
+++ src/app/angular/components/analysis/type/type.component.ts
@@ -1,17 +1,54 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output
+} from "@angular/core";
+import { AnalysisType } from "../../../../electron/models/analysis";
 import { APService } from "../../../../electron/services/AP/ap.service";
 
 @Component({
   selector: "app-analysis-type",
   templateUrl: "./type.component.html",
-  styleUrls: ["./type.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ["./type.component.scss"]
 })
 export class TypeComponent implements OnInit {
+  @Input() backButton?: boolean;
+  @Output() analysisTypeEvent = new EventEmitter<AnalysisType>();
+
+  private analysisTypeCurrent: AnalysisType;
+  private analysisTypeOptions: AnalysisOption[];
+
   constructor(private apService: APService) {}
 
   ngOnInit(): void {
-    console.log("Type Selector");
-    console.log(this.apService.getAnalysisTypes());
+    this.analysisTypeOptions = this.apService
+      .getAnalysisTypes()
+      .map(analysisType => {
+        return {
+          analysis: analysisType,
+          isSelected: false
+        } as AnalysisOption;
+      });
   }
+
+  /**
+   * Set the selected analysis type
+   * @param id ID of analysis option
+   */
+  changeSelection(id: number) {
+    this.analysisTypeCurrent = this.analysisTypeOptions[id].analysis;
+    this.analysisTypeOptions.map((analysisOption, index) => {
+      analysisOption.isSelected = index === id;
+    });
+
+    this.analysisTypeEvent.emit(this.analysisTypeCurrent);
+  }
+}
+
+interface AnalysisOption {
+  analysis: AnalysisType;
+  isSelected: boolean;
 }
