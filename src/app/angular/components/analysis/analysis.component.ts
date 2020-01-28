26e130c24a526bfcda761ac8157bedb9ad43ab29
Charles Alleman <charles@alleman.com.au>
2020-01-28 13:28:18 +1000

    Removed test terminal

--- src/app/angular/components/analysis/analysis.component.ts
+++ src/app/angular/components/analysis/analysis.component.ts
@@ -38,23 +38,6 @@ export class AnalysisComponent implements OnInit {
 
     this.isValid = false;
     this.backEnabled = this.wizard.getAnalyses().count() > 0;
-
-    const terminal = this.wizard.childProcess.spawn(
-      "C:/Users/Charles/ap/AnalysisPrograms.exe",
-      [
-        "audio2csv",
-        "D:/Desktop/AP Test Audio/F_014S01_20100803_140000.wav",
-        "C:/Users/Charles/ap/ConfigFiles/Towsey.Acoustic.yml",
-        "D:/Documents/AP Desktop/Basic Analysis/F_014S01_20100803_140000",
-        "--parallel"
-      ]
-    );
-
-    terminal.stdout.on("data", data => console.log("Data: ", data.toString()));
-    terminal.stderr.on("error", error =>
-      console.error("Error: ", error.toString())
-    );
-    terminal.on("close", code => console.log("Code: ", code));
   }
 
   public changeSelection(index: number): void {
