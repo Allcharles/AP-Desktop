cd9a3bf1a0936b80ae5c7cc3bfb1b30da39ee6e4
Charles Alleman <charles@alleman.com.au>
2020-01-29 18:04:32 +1000

    Changed defaultAnalyses to JSON

--- src/app/electron/models/defaultAnalyses.ts
+++ src/app/electron/models/defaultAnalyses.ts
@@ -1,69 +1,38 @@
-import { APAnalysis } from "./analysis";
 import {
+  AnalysisConfigFile,
   AnalysisOption,
   AnalysisOptions,
   AnalysisProcessingType,
   Enabled
 } from "./analysisHelper";
 
-export const DEFAULT_OPTIONS: AnalysisOptions = {
+const defaultEventDetectorChanges = {
+  SaveIntermediateWavFiles: "WhenEventsDetected",
+  SaveSonogramImages: "WhenEventsDetected"
+};
+const defaultOptions: AnalysisOptions = {
   [AnalysisOption.mixDownToMono]: Enabled.True,
   [AnalysisOption.parallel]: true
 };
 
-// TODO Update this to read from AP's AnalysesAvailable command
-export const defaultAnalyses: APAnalysis[] = [
-  new APAnalysis(
-    AnalysisProcessingType.audio2csv,
-    "Basic Analysis",
-    {
+export const defaultAnalyses: {
+  type: AnalysisProcessingType;
+  label: string;
+  configFile: AnalysisConfigFile;
+  shortDescription: string;
+  description: string;
+  options: AnalysisOptions;
+}[] = [
+  {
+    type: AnalysisProcessingType.audio2csv,
+    label: "Basic Analysis",
+    configFile: {
       template: "Towsey.Acoustic.yml",
       changes: {}
     },
-    "Basic visual analysis of audio.",
-    "[BETA] Generates all our default summary & spectral acoustic indices. Also generates false color spectrograms IFF IndexCalculationDuration==60.0.",
-    { ...DEFAULT_OPTIONS }
-  ),
-  new APAnalysis(
-    AnalysisProcessingType.audio2csv,
-    "Crow Event Detection",
-    {
-      template: "Towsey.Crow.yml",
-      changes: {
-        SaveIntermediateWavFiles: "WhenEventsDetected",
-        SaveSonogramImages: "WhenEventsDetected"
-      }
-    },
-    "Automatic detection of crow 'caw' sounds.",
-    "Automatic detection of crow 'caw' sounds in audio files GREATER than 5 minutes long. This combines with the EventDetection utility under the Utilities tab.",
-    { ...DEFAULT_OPTIONS }
-  ),
-  new APAnalysis(
-    AnalysisProcessingType.audio2csv,
-    "Human Event Detection",
-    {
-      template: "Towsey.Human.yml",
-      changes: {
-        SaveIntermediateWavFiles: "WhenEventsDetected",
-        SaveSonogramImages: "WhenEventsDetected"
-      }
-    },
-    "Automatic detection of human voices.",
-    "Automatic detection of human (male/female) voices in audio files GREATER than 5 minutes long. This combines with the EventDetection utility under the Utilities tab.",
-    { ...DEFAULT_OPTIONS }
-  ),
-  new APAnalysis(
-    AnalysisProcessingType.audio2csv,
-    "Koala Event Detection",
-    {
-      template: "Towsey.KoalaMale.yml",
-      changes: {
-        SaveIntermediateWavFiles: "WhenEventsDetected",
-        SaveSonogramImages: "WhenEventsDetected"
-      }
-    },
-    "Automatic detection of koala sounds.",
-    "Automatic detection of koala sounds in audio files GREATER than 5 minutes long. This combines with the EventDetection utility under the Utilities tab.",
-    { ...DEFAULT_OPTIONS }
-  )
+    shortDescription: "Basic visual analysis of audio.",
+    description:
+      "[BETA] Generates all our default summary & spectral acoustic indices. Also generates false color spectrograms IFF IndexCalculationDuration==60.0.",
+    options: defaultOptions
+  }
 ];
